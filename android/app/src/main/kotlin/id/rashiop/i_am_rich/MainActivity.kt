75de06cacc7ad35df537ba93cf16d615fc1c5d40
Poppy Sari <poppy.sari@rga.com>
2021-10-23 21:47:55 +0700

    :rocket: bump

---
+++ android/app/src/main/kotlin/id/rashiop/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.rashiop.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.redAccent[100],
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(image: AssetImage('images/owl.jpeg')),
+        ),
+      ),
+    ),
+  );
+}
